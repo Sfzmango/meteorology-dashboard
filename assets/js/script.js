584c978ac869994ef576e7ac05574d557b94ef3e
Sfzmango <maunghtike1999@gmail.com>
2022-04-09 16:31:47 -0700

    link geocoding api and restrict r-searches to 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 // function for dynamically creating a button for recent searches on clicking the search button
 $("#searchBtn").on("click", function (event) {
     event.preventDefault();
@@ -5,9 +6,36 @@ $("#searchBtn").on("click", function (event) {
     // stores value in search text box as a variable
     var textEl = $("#searchBox").val();
 
-    // dynamically creates a button for recent searches
-    $("#recentSearches").append('<li class="list-group-item"><button type="button" class="btn btn-primary btn-block w-100">' + textEl + '</button></li>');
+    // dynamically creates a button for recent searches. checks if the textbox contains value.
+    if (textEl) {
+        // request geolocation using geolocation api from open weather api
+        var requestWeatherUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + textEl + "&appid=0838a1f6c730425e60b39b77d73f4aad";
+        // clears textbox after clicking the search button
+        fetch(requestWeatherUrl)
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                // limits total recent searches up to 5
+                if (data.length !== 0 && $("#recentSearches").children().length < 5) {
+                    $("#recentSearches").append('<li class="list-group-item"><button type="button" class="btn btn-primary btn-block w-100">' + textEl + '</button></li>');
+                    return data;
+                }
+                // if there is more than 5 searches, it will delete 
+                else if ($("#recentSearches").children().length === 5) {
+                    $("#recentSearches").children().last().remove();
+                    $("#recentSearches").prepend('<li class="list-group-item"><button type="button" class="btn btn-primary btn-block w-100">' + textEl + '</button></li>');
+                    return data;
+                }
+                // logs out error if location does not exist
+                else {
+                    console.log("error");
+                    return;
+                }
+            })
+    }
 
-    // clears textbox after clicking the search button
     $("#searchBox").val("");
-});
+}
+);
